390a1add7a48802df53677136791b8bdb8012002
salemi <en.salemi75@gmail.com>
2023-06-21 15:08:17 +0330

    style: fix input style for english

--- src/features/authentication/components/form-input/index.tsx
+++ src/features/authentication/components/form-input/index.tsx
@@ -3,6 +3,7 @@ import { FormInputProps } from "./type";
 import { FaMobile, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 import styles from "./form-input.module.css";
+import { useAppContext } from "contexts/app";
 
 const FormInput = <TFormValues extends FieldValues>(
   props: FormInputProps<TFormValues>
@@ -19,6 +20,9 @@ const FormInput = <TFormValues extends FieldValues>(
   } = props;
   const errorMessage = error?.[name]?.message;
   const [showPassword, setShowPassword] = useState(false);
+  const {
+    state: { language },
+  } = useAppContext();
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -53,14 +57,19 @@ const FormInput = <TFormValues extends FieldValues>(
     <div className={`${styles.wrapper} ${isErrorEmpty && styles["mb-2"]}`}>
       <div className={styles["input-wrapper"]}>
         <div className={styles["right-icon"]}>{renderRightIcon()}</div>
-        <div className={styles["left-icon"]}>{renderLeftIcon()}</div>
+        <div
+          className={
+            styles[language === "fa" ? "right-eye-icon" : "left-eye-icon"]
+          }
+        >
+          {renderLeftIcon()}
+        </div>
         <input
           name={name}
           placeholder={placeholder}
           className={`${styles["input-field"]} form-control ${
-            error?.[name] && "is-invalid"
-          }`}
-          style={{ width: "100%", height: "42px", paddingRight: "1.6rem" }}
+            styles[language === "fa" ? "pr" : "pl"]
+          } ${error?.[name] && "is-invalid"}`}
           type={inputType}
           {...rest}
           {...(register && register(name))}
